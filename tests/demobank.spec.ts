205242b8ca7d9a7071b7fe44b00e91fc89b2b7c4
Vladimir Karpach <vladimir.karpach@gmail.com>
2025-02-14 12:32:57 +0100

    Flaky test adjusted. Docker added

--- tests/demobank.spec.ts
+++ tests/demobank.spec.ts
@@ -43,6 +43,7 @@ test.describe('Login Page', () => {
 
         await onLoginPage.passwordInputField.fill(credentials.correctPassword);
         await onLoginPage.signInButton.click({force: true});
+        await page.waitForTimeout(100)
         await onLoginPage.checkFieldHighlight('password', 'is-valid')
     })
 
