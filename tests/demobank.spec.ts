7ab7c6eafe375b3c377779903deaaf8c3184f157
Vladimir Karpach <vladimir.karpach@gmail.com>
2025-02-15 17:32:19 +0100

    Trying to fix flaky test

--- tests/demobank.spec.ts
+++ tests/demobank.spec.ts
@@ -44,6 +44,7 @@ test.describe('Login Page', () => {
 
         await onLoginPage.passwordInputField.fill(credentials.correctPassword);
         await onLoginPage.signInButton.click({force: true});
+        await page.waitForTimeout(500);
         await onLoginPage.checkFieldHighlight('password', 'is-valid');
     })
 
